85e71e96fb91c6903ebdd2edd183ea3120b0f3c0
unknown <binayakchaudhary00@gmail.com>
2026-05-24 02:14:37 +0530

    fix port

--- App.py
+++ App.py
@@ -95,4 +95,5 @@ def wish(session_id):
         photos_json=json.dumps(photos_b64))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
